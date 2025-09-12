39f3af4bd1c748231548cda760db89d54950619d
Eyly408 <eyleenyrigoyn14@gmail.com>
2025-09-11 22:55:13 -0500

    primer ejercicio

--- Program.cs
+++ Program.cs
@@ -16,6 +16,40 @@ namespace mi_proyecto
             //probando commit 2
 
             Console.WriteLine("Bienvenidos");
+
+            //Primer ejercicio
+            Double sis;
+            Double adm;
+            Double cont;
+            Double total;
+            Double porSis;
+            Double porAdm;
+            Double porCont;
+
+            Console.WriteLine("Ingrese la cantidad de alumnos en Ingenieria de sistemas");
+            sis = Convert.ToDouble(Console.ReadLine());
+            Console.Beep(900, 500);
+            Console.WriteLine("Ingerese la cantidad de alumnos en Administracion");
+            adm = Convert.ToDouble(Console.ReadLine());
+            Console.Beep(900, 500);
+            Console.WriteLine("Ingrese la cantidad de alumnos en Contabilidad");
+            cont = Convert.ToDouble(Console.ReadLine());
+            Console.Beep(900, 500);
+            total = (sis + adm + cont);
+            porSis = (sis / total) * 100;
+            porAdm = (adm / total) * 100;
+            porCont = (cont / total) * 100;
+            Console.WriteLine($"El porcentaje de alumnos en Ingenieria de sistemas es {porSis}.");
+            Console.WriteLine($"El porcentaje de alumnos en Administracion es {porAdm}.");
+            Console.WriteLine($"El porcentaje de alumnos en Contabilidad es {porCont}.");
+
+            Console.ReadKey();
+
+            
+            
+
+            
+
         }
     }
 }
