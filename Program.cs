a38cdb94d02b3b52e8edbbf3434b7b5096c4da33
Eyly408 <n00478352@upn.pe>
2025-09-11 22:03:32 -0500

    Add files via upload
    .

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignación_3384
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            //Comentario agregado para probar commit
+        }
+    }
+}
