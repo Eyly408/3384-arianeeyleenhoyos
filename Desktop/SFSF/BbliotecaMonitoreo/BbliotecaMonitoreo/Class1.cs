4fd0f850d514f108ea695ca7edc071576099ea85
Eyly408 <eyleenyrigoyn14@gmail.com>
2025-11-23 11:57:19 -0500

    //Commit 1 de biblioteca

---
+++ Desktop/SFSF/BbliotecaMonitoreo/BbliotecaMonitoreo/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BbliotecaMonitoreo
+{
+    public class Class1
+    {
+        private Random rnd = new Random();
+
+        public int ObtenerTemperatura()
+        {
+            return rnd.Next(15, 81);
+        }
+
+        public double ObtenerHumo(int temperatura)
+        {
+            double num = (double)temperatura * 1.2 + (double)rnd.Next(-10, 11);
+            if (num < 0.0)
+            {
+                num = 0.0;
+            }
+
+            if (num > 100.0)
+            {
+                num = 100.0;
+            }
+
+            return Math.Round(num, 1);
+        }
+
+    }
+}
